a8e8c7adeba8a02d47cacfc87eb58e53cb6607ff
KAKAURIDZE-N-ONE <g_kakauridze3@cu.edu.ge>
2025-01-23 18:59:45 +0400

    fix/cs-fixer

--- app/Http/Resources/QuizResource.php
+++ app/Http/Resources/QuizResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class QuizResource extends JsonResource
 {
@@ -15,19 +16,19 @@ class QuizResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'image' => url('/storage/'.$this->image),
+            'image' => url(Storage::url($this->image)),
             'title' => $this->title,
             'description' => $this->description,
             'duration' => $this->duration,
             'total_filled' => $this->total_filled,
             'level_id' => $this->level_id,
             'created_at' => $this->created_at,
-            'updated_at	' => $this->updated_at	,
+            'updated_at' => $this->updated_at,
             'instructions' => $this->instructions,
             'questions' => $this->questions,
             'level' => $this->level,
             'categories' => $this->categories,
-            'users' => $this->users
+            'users' => $this->users,
         ];
     }
 }
